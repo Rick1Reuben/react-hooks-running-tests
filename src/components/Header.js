ac41942902f2b514fdf0255983b4e7920089ab1e
reuben <rickr6129@gmail.com>
2024-04-24 18:20:21 +0300

    Understood Lab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Article from "./Article";
 
 function Header(props) {
-      return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
+  return (
+    <div>
+      <h1>
+        hello from the Header!
+      </h1>
+      <Article />
+    </div>
+  );
 }
 
 export default Header;
